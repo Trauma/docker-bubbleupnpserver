e479116d90908e821f16a64940db01519d3dd98b
tromatik <tromatik@gmail.com>
2015-08-31 23:03:53 +0200

    Not needed anymore

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-export DEBIAN_FRONTEND=noninteractive
-
-java -Xss256k -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding="UTF-8" -jar /usr/share/bubbleupnpserver/BubbleUPnPServerLauncher.jar
